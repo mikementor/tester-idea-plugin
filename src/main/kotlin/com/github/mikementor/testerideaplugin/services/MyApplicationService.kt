0933ec35a3f60efedf20527d66d4c370f6c9f7cd
GitHub Action <action@github.com>
2020-12-17 21:12:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mikementor/testerideaplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mikementor.testerideaplugin.services
+
+import com.github.mikementor.testerideaplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
